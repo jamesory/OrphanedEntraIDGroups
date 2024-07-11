6ef246fb4fc894847fdef70d3cfe0ec68071ee7a
jamesory <95860734+jamesory@users.noreply.github.com>
2024-07-11 17:14:10 -0500

    Create orphanedEntraIDGroup.ps1

---
+++ orphanedEntraIDGroup.ps1
@@ -0,0 +1,36 @@
+#Install the Micrsoft Graph Powershell Module
+Install-Module Microsoft.Graph -Scope AllUsers
+
+#Connect To the Microsoft Graph
+Connect-MgGraph -Scopes 'Group.Read.All'
+
+#Get Groups that Are OnPremise Synced, Avoiding Office365 Groups
+
+$Azure = Get-MgGroup -Filter "OnPremisesSyncEnabled eq true" 
+
+# Get Active Directory Groups
+
+$AD = get-adgroup -filter * 
+# Parse the Groups and Place EntraID Display Name and Active Directory SamAccountName into an Array
+
+$AZDN=$Azure.displayname
+$ADDN=$AD.SamAccountName
+
+# Compare EntraID to Active Directory, if a group is not in Active Directory, it is more than likely Orphaned 
+$Groups = $AZDN| Where-Object { $ADDN -Contains $_ }
+
+# Obtain the EntraID Group ID, which is needed to delete the group
+$objectid = foreach ($id in $Groups) {
+    Get-MgGroup -filter "DisplayName eq '$id'" 
+ }
+
+# Put the ID into an Array
+$objectid2=$objectid.Id
+
+# Connect to EntraID and remove the groups
+Connect-AzureAD
+
+foreach ($id2 in $objectid2) {
+    Remove-AzureADGroup -ObjectId "$id2"
+ }
+
